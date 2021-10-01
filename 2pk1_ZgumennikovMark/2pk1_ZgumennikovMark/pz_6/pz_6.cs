f1ca152a5c1a2f401aa3e3487adba9d3700ef2d9
Zagumennikov_Mark <zagumennikovmark@mail.ru>
2021-10-01 08:21:15 +0500

    Практика pz_6 Загуменников Марк

---
+++ 2pk1_ZgumennikovMark/2pk1_ZgumennikovMark/pz_6/pz_6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 1;
+            while (k<=50)
+            {
+                Console.Write(k+" ");
+                k *= 2;
+            }
+        }
+    }
+}
