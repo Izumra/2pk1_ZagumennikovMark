be0736868ed5592ce4e1107da845e0068c23bf27
UchihaItach <91648199+UchihaItach@users.noreply.github.com>
2021-10-15 10:20:12 +0500

    Add files via upload

---
+++ pz_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = new int[12];
+            Random rnd = new Random();
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i <= 11; i++)
+            {
+                massive[i] = rnd.Next(-12, 12);
+                Console.WriteLine(massive[i]);
+            }
+            Console.WriteLine("Массив со сдвигом:");
+            int[] p = new int[4] { massive[0], massive[1], massive[2], massive[3] };
+            for (int j = 0; j <= 7; j++)
+            {
+                massive[j] = massive[j + 4];
+                Console.WriteLine(massive[j]);
+            }
+            for (int r = 0; r <= 4; r++){Console.WriteLine(p[r]);}
+        }
+    }
+}
