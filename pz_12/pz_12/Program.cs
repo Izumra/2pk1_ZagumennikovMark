499304cf304495893b79862a60dc64152424bd96
UchihaItach <91648199+UchihaItach@users.noreply.github.com>
2021-11-11 19:17:43 +0500

    Add files via upload

---
+++ pz_12/pz_12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] values = text.Split(new char[] { ' ' });
+            for (int i = 0; i < values.Length; i++)
+            {
+                try
+                {
+                    if (Convert.ToInt32(values[i]) == 0||0 / Convert.ToInt32(values[i]) == 0)
+                    {
+                        Console.WriteLine(Convert.ToInt32(values[i]));
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
