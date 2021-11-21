8ac7f6192103b27f00656c484915494a03bad30c
UchihaItach <91648199+UchihaItach@users.noreply.github.com>
2021-11-21 13:19:24 +0500

    Add files via upload

---
+++ pz_15/pz_15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(@"C:\Files\File_1.txt", FileMode.Open,FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            string line;
+            string maxString = "";
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Length > maxString.Length)
+                {
+                    maxString = line;
+                }
+                    
+            }
+            Console.WriteLine(maxString);
+
+            reader.Close();
+        }
+    }
+}
